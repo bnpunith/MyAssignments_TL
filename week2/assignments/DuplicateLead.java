d29971939f64b69968956aa9a7e0b29479b6b979
mr-punithbn <118036329+mr-punithbn@users.noreply.github.com>
2023-02-17 00:00:05 +0530

    Add files via upload

---
+++ week2/assignments/DuplicateLead.java
@@ -0,0 +1,53 @@
+package week2.assignments;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class duplicateLead {
+	
+	public static void main(String[] args) throws InterruptedException 
+	{
+		ChromeDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("http://leaftaps.com/opentaps/control/login");
+		driver.findElement(By.id("username")).sendKeys("demosalesmanager");
+		driver.findElement(By.id("password")).sendKeys("crmsfa");
+		driver.findElement(By.className("decorativeSubmit")).click();
+		driver.findElement(By.linkText("CRM/SFA")).click();
+		driver.findElement(By.linkText("Leads")).click();
+		driver.findElement(By.linkText("Create Lead")).click();
+		driver.findElement(By.id("createLeadForm_companyName")).sendKeys("Deloitte");
+		driver.findElement(By.id("createLeadForm_firstName")).sendKeys("Punith");
+		driver.findElement(By.id("createLeadForm_lastName")).sendKeys("Gowda");
+		driver.findElement(By.id("createLeadForm_firstNameLocal")).sendKeys("Puni");
+		driver.findElement(By.id("createLeadForm_departmentName")).sendKeys("Testing");
+		driver.findElement(By.id("createLeadForm_description")).sendKeys("Testing the app");
+		driver.findElement(By.id("createLeadForm_primaryEmail")).sendKeys("punith@gmail.com");
+		
+		WebElement dd=driver.findElement(By.name("generalStateProvinceGeoId"));
+		Select sel = new Select(dd);
+		sel.selectByVisibleText("New York");
+		driver.findElement(By.name("submitButton")).click();
+		String title=driver.getTitle();
+		System.out.println("Title is "+title);
+		
+		driver.findElement(By.linkText("Duplicate Lead")).click();
+		driver.findElement(By.id("createLeadForm_companyName")).clear();
+		driver.findElement(By.id("createLeadForm_companyName")).sendKeys("PWC");
+		
+		
+		driver.findElement(By.id("createLeadForm_firstName")).clear();
+		driver.findElement(By.id("createLeadForm_firstName")).sendKeys("Amith");
+		
+
+		driver.findElement(By.name("submitButton")).click();
+		
+		Thread.sleep(1500);
+		String title2 = driver.getTitle();
+		System.out.println("Title 2 is "+title2);
+		driver.close();
+	}
+
+}
